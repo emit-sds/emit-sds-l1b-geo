56ffd77bcd8453ecc949ae70be85d267f8fc873b
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2021-02-27 15:46:53 -0800

    Put generation of LOC into a top level program l1b_geo_process. Have place holders for all the other output files. Need to figure out the GLT with geographic information we talked with Phil about, we'll need to find the file name and actually include it in our processing.

--- python/lib/l1b_geo_generate.py
+++ python/lib/l1b_geo_generate.py
@@ -1,6 +1,11 @@
 from .envi_file import EnviFile
 import geocal
 import numpy as np
+import os
+import shutil
+import logging
+
+logger = logging.getLogger('l1b_geo_process.l1b_geo_generate')
 
 # Not sure if we really want this to be a class or not, but I think there
 # may be enough overlap between calculations that we want to keep these
@@ -9,13 +14,67 @@ import numpy as np
 # Also, we'll probably want to run some of this in parallel and/or move to
 # C++. I imagine this will be a bit slow.
 class L1bGeoGenerate:
-    def __init__(self, igc):
+    def __init__(self, igc, prod_dir):
         self.igc = igc
+        self.prod_dir = prod_dir
 
-    def generate_loc(self, loc_fname):
+    def emit_file_name(self, file_type, ext=".img"):
+        '''Generate a EMIT file name using the EMIT naming convention'''
+        # For now, just use the file_type. We'll add the rest later
+        return os.path.join(self.prod_dir, file_type + ext)
+
+    def generate_quicklook(self, fname):
+        '''Generate the quick look image file'''
+        # Currently just a fake file
+        logger.info("Generating Quicklook data")
+        with open(fname, "w") as fh:
+            print("Fake data", file=fh)
+
+    def generate_kmz(self, fname):
+        '''Generate KMZ quick look image file'''
+        # Currently just a fake file
+        logger.info("Generating KMZ data")
+        with open(fname, "w") as fh:
+            print("Fake data", file=fh)
+
+    def generate_qa(self, fname):
+        '''Generate QA file'''
+        # Currently just a fake file
+        logger.info("Generating QA data")
+        with open(fname, "w") as fh:
+            print("Fake data", file=fh)
+
+    def generate_att(self, fname):
+        '''Generate a corrected ATT file'''
+        # Currently just a place holder, we copy the input file to the output
+        logger.info("Generating ATT data")
+        fin = self.igc.ipi.orbit.file_name
+        shutil.copyfile(fin, fname)
+
+    def generate_obs(self, fname):
+        '''Generate the Level 1B Observation Geometry (OBS) File'''
+        logger.info("Generating OBS data")
+        d = EnviFile(fname,
+                     shape=(11, self.igc.number_line, self.igc.number_sample),
+                     dtype = np.float64, mode="w")
+        d[:,:,:] = -999
+
+    def generate_glt(self, fname):
+        '''Generate the Level 1B Geographic Lookup Table (GLT) File'''
+        # TODO Note we talked with Phil about generating 2 versions of these, one
+        # with a rotated UTM and one with rotated geographic. We'll need to
+        # come back to this, and figure out the file names
+        logger.info("Generating GLT data")
+        d = EnviFile(fname,
+                     shape=(2, self.igc.number_line, self.igc.number_sample),
+                     dtype = np.int32, mode="w")
+        d[:,:,:] = -999
+        
+    def generate_loc(self, fname):
         '''Generate the Level 1B Pixel Location (LOC) File'''
         # This will likely be a bit on the slow side
-        d = EnviFile(loc_fname,
+        logger.info("Generating LOC data")
+        d = EnviFile(fname,
                      shape=(3, self.igc.number_line, self.igc.number_sample),
                      dtype = np.float64, mode="w")
         # We pick a large resolution here to force the subpixels to be 1.
---
+++ python/lib/l1b_geo_log_formatter.py
@@ -0,0 +1,36 @@
+import logging
+
+logger = logging.getLogger('nitf_diff')
+
+class L1bGeoLogFormatter(logging.Formatter):
+    '''Set logging format, optionally with color'''
+    def __init__(self, add_color = True):
+        self.add_color = add_color
+
+    def color_text(self, text, levelno):
+        # ANSI colors
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        if(not self.add_color):
+            return text
+        if(levelno == logging.DEBUG):
+            return OKBLUE + text + ENDC
+        elif(levelno == logging.INFO):
+            return OKGREEN + text + ENDC
+        elif(levelno == logging.WARNING):
+            return WARNING + text + ENDC
+        elif(levelno == logging.ERROR):
+            return FAIL + text + ENDC
+        elif(levelno == logging.CRITICAL):
+            return FAIL + text + ENDC
+        return text
+    def format(self, record):
+        return (self.color_text(record.levelname + ": ", record.levelno) +
+                record.getMessage())
+        
